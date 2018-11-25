ddbb1170e92a26734cae8348638ef21b4a15e9af
Katricia Smith <smithkatricia@gmail.com>
2018-11-25 13:05:24 -0800

    Add pseudocode for nested loop solution. Comment on possible hash solution

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,22 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  # Verify neither array is empty or nil
+
+  # Set smaller array as values to compare
+
+  # Instantiate new array for intersection
+
+  # For each element in first array
+
+  # Check against each of second array
+
+  # If find match, push to intersection array
+
+  # Return array of intersection
 end
+
+
+# Nested loop O(n^2) ?
+
+
+# Second solution is through hash table?
